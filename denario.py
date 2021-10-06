f242c0d8eefdcd6b1e4ddc16e1edd6d8f1145d53
Toulousegg <franklin.m.mogollon@gmail.com>
2021-10-06 08:35:48 -0400

    convertir moneda (beta system)

---
+++ denario.py
@@ -0,0 +1,4 @@
+cantidad = float(input('Digame una cantidad de Denarios '))
+print (f'{cantidad} denarios son: {round (0.76 * cantidad)} $')
+#cantidad = float(input("Dígame una cantidad de Denarios: "))
+#print(f"{cantidad} Denarios son {round (0.76 * cantidad)} Dolares")
